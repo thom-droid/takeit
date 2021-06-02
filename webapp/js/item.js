6a8ee81609b9b62c1d932600dab23bf873c70b13
Thom <hsbang.thom@gmail.com>
2021-06-02 22:04:48 +0900

    changed some codes for storage to make it more readable

--- webapp/js/item.js
+++ webapp/js/item.js
@@ -1,16 +1,31 @@
-/**
- * 
- */
-/* ======================탭 내용 & css 변경========================= */
 
-// 배송 옵션에 따른 가격 변동
+// delivery option
 const $optArea = $(".opt_area");
 
-// 배송지 옵션 선택 + 가격 변동
+// tab contents 
+const $tab = $(".product_detail_tab_box");
+const $content = $(".content");
 
+// datepicker
+let minDateInput = document.getElementById("dateVal");
+let dateVal = minDateInput.value;
+
+
+// rating 
+let allRatings = document.querySelectorAll(".rating");
+let ratingSum = 0;
+let rating = ratingSum/allRatings.length;
+
+
+// delivery option 
 $optArea.on("click", ".delivery_opt_label", function() {
-	$(".total_price_input").val(this.dataset.price);
+	
 	$(".product_price").text(this.dataset.price);
+	$(".opt_price_input").val(this.dataset.optPrice);
+	$(".opt_region1_input").val(this.dataset.sido);
+	$(".opt_region2_input").val(this.dataset.sigungu);
+	$(".opt_no").val(this.dataset.optNo);
+	$(".opt_area_no").val(this.dataset.optAreaNo);
 
 	// 시도 / 시군구
 	curAddrOptSido = this.dataset.sido;
@@ -27,12 +42,8 @@ $optArea.on("click", ".delivery_opt_label", function() {
 
 });// on click
 
-/* 탭 */
-const $tab = $(".product_detail_tab_box");
-/* 탭 안의 컨텐츠 */
-const $content = $(".content");
 
-/* 탭 클릭 시 css 변경 & 변경된 탭 내용 보여주기 */
+// tab display action
 $tab.on("click", function() {
 	$tab.removeClass("on");
 	$(this).addClass("on");
@@ -46,10 +57,8 @@ $tab.on("click", function() {
 }); //$tab click() end
 
 
-/* ==========================datepicker 이용================================= */
 
-/* datepicker 한글로 변경 */
-
+// datepicker setup
 $.datepicker.setDefaults({
 	dateFormat: 'yy-mm-dd',
 	prevText: '이전 달',
@@ -63,43 +72,59 @@ $.datepicker.setDefaults({
 	yearSuffix: '년'
 });
 
-let minDateInput = document.getElementById("dateVal");
-let dateVal = minDateInput.value;
-console.log(dateVal);
+
 $(function() {
 
 	//오늘 날짜를 출력
 	$("#today").text(new Date().toLocaleDateString());
 
 	$("#datepicker").datepicker({
-		/*
-			선택할 수 있는 최소 날짜 (3이면 현재 날짜로부터 3일 이후부터 가능)
-			=> 나중에 서버에서 기버가 배송 가능한 날짜 선택하면
-			그 값 받아서 나타내면 될 듯!
-		 */
+		
+		// date range available. 2 weeks from today
 		minDate: dateVal,
-		/* 뭔가 이거는 다같이 얘기 해봐야 하지 않을까? 최대 기간 정해두는 게 나을 것 같다. */
 		maxDate: 14
 	});
 
 }); //function() end
 
-let allRatings = document.querySelectorAll(".rating");
 
-let ratingSum = 0;
-
+// count the numbers of reviews
 allRatings.forEach(item =>{
 	
 	ratingSum += parseInt(item.textContent);
 	
 });
 
-let rating = ratingSum/allRatings.length;
-console.log(rating);
-console.log(ratingSum);
-console.log(allRatings.length);
-
 document.querySelector(".star_review").textContent = rating;
-
 document.querySelector(".rating_num").innerHTML = allRatings.length; 
+
+// validation test
+const submitBtn = document.querySelector(".subscribeBtn");
+
+submitBtn.addEventListener("click", (e)=>{
+	
+	let dateVal = document.getElementById("datepicker").value;
+	let regionVal = document.getElementById("primaryRegion").value;
+	let optPriceVal = document.getElementById("optPrice").value;
+	
+	if(!dateVal){
+		alert("choose the date!");
+		e.preventDefault();
+	} else{
+		alert(`${dateVal} is chosen`);
+	}
+	
+	if(!regionVal || !optPriceVal){
+		alert("choose the region!");
+		e.preventDefault();
+		
+	} else if(regionVal && optPriceVal){
+		
+		alert(`지역 :  ${regionVal}, 추가가격 : ${optPriceVal}`);
+	}
+	
+	
+});
+
+
 
